824f1c936c5ec6c03fa59d9585e2aa60e3665931
Wex <jomagranmagno@gmail.com>
2025-04-07 15:12:25 +0800

    change link of booke now

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ styles }) => (
-  <a href="https://e-catsulta.vercel.app/" target="_self" rel="noopener noreferrer">
+  <a href="https://book-ecatsulta.com/" target="_self" rel="noopener noreferrer">
     <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
       Book Now
     </button>
